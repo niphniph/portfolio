ea0897ceaefd80872dcf04e22bdcb8059e6b9e2d
Nino Kapanadze <ninekapanadze@gmail.com>
2026-06-23 19:57:28 +0400

    build: configure base path to /portfolio/ for subdirectory hosting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const portfolioBaseRedirect = () => ({
+  name: 'portfolio-base-redirect',
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url === '/portfolio' || req.url?.startsWith('/portfolio?')) {
+        res.statusCode = 302
+        res.setHeader('Location', req.url.replace('/portfolio', '/portfolio/'))
+        res.end()
+        return
+      }
+      next()
+    })
+  },
+  configurePreviewServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url === '/portfolio' || req.url?.startsWith('/portfolio?')) {
+        res.statusCode = 302
+        res.setHeader('Location', req.url.replace('/portfolio', '/portfolio/'))
+        res.end()
+        return
+      }
+      next()
+    })
+  },
+})
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './'
+  plugins: [react(), portfolioBaseRedirect()],
+  base: '/portfolio/'
 })
